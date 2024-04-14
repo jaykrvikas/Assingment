4ce2c260b3f84ab5d37fd5c269b87a2e2996d089
Vikas Kumar Singh <113041034+jaykrvikas@users.noreply.github.com>
2024-04-14 22:11:55 +0530

    Add files via upload

---
+++ Day2Assingment.js
@@ -0,0 +1,80 @@
+//problem 1. Print "Ducat An IT Institute" in the console followed by "A Transformation in Education" in the next line.
+
+console.log("Ducat An IT Institute")
+console.log("A Transformation in Education ")
+
+
+/* problem 2.
+     Define a variable called  name  with your Name as the assigned value
+     Print the value stored in the variable name
+     Change the variable to store your father's name
+     Print the value stored in the variable name
+     Change the variable again to store your mother's name.
+     Print the value stored in the variable name
+*/
+console.log("solution 2")
+console.log("------------")
+var name = "Vikas Kumar Singh"
+console.log(name)
+name = "Manoj Kumar Singh"
+console.log(name)
+name = "Geeta Singh"
+console.log(name)
+
+
+/*problem 3 
+    Create a variable  name  to store your name
+    Create a variable  age  to store your age
+    Print the values stored in the variable on one line followed by the type of the variable in the next line
+*/
+
+console.log("solution 3")
+console.log("------------")
+var Name = "Vikas Kumar Singh", age = 45
+console.log(Name)
+console.log(age)
+
+/* problem 4 
+    Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+    Print the report card of the student (You can make it look nice by using some keyboard symbols)
+*/
+
+console.log("solution 4")
+console.log("------------")
+console.log()
+console.log()
+
+console.log("+---------------------------------------------------+")
+console.log("|        S GYAN BHARTI PUBLIC SCHOOL                |")
+console.log("|                                                   |")
+console.log("|                REPORT CARD                        |")
+console.log("|                                                   |")
+console.log("| Student Name: John Wick                           |")
+console.log("| Class Level: X (10th Grade)                       |")
+console.log("| Identification Number: 5371892                    |")
+console.log("|                                                   |")
+console.log("| Academic Performance Overview:                    |")
+console.log("| ------------------------------------------------- |")
+console.log("| Subject                | Assessment | Proficiency |")
+console.log("| ---------------------- | ---------- | ----------- |")
+console.log("| Advanced Mathematics   | A          | Excellent   |")
+console.log("| Science                | B+         | Very Good   |")
+console.log("| Social Studies         | A-         | Good        |")
+console.log("| Hindi                  | A-         | Good        |")
+console.log("| Comprehensive English  | B          | Satisfactory|")
+console.log("| Elective Language      | A          | Excellent   |")
+console.log("|                                                   |")
+console.log("| Cumulative Grade Point Average: 8.1               |")
+console.log("|                                                   |")
+console.log("| Educator's Commentary:                            |")
+console.log("| John has shown remarkable improvement this term   |")
+console.log("| and has excelled in Advanced Mathematics and      |")
+console.log("| Elective Language. Continued effort in Science    |")
+console.log("| and English is encouraged.                        |")
+console.log("|                                                   |")
+console.log("|  |}-/|       ****************************   (}-/) |")
+console.log("| / @ @ }      *        keep it up        *  (='.'=)|")
+console.log("|( > º < )     *        Doing Good        *         |")
+console.log("| `>>x<<´      *   (sgyanbhp@gmail.com)   *         |")
+console.log("| /  O  }      ****************************         |")
+console.log("+---------------------------------------------------+")
